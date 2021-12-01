15c6e60fd04558103894610fc8399b84493b2ccb
Jacob Trevithick <jaketrevithick@Jakes-MBP.attlocal.net>
2021-11-30 17:05:04 -0800

    Building out of Flask app and database communication. Outline of Visualizations made

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, jsonify
 import config
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -18,11 +18,11 @@ Base = automap_base()
 Base.prepare(engine, reflect = True)
 
 # create table objects
-countries = Base.classes.countries
-greenhouse_gases = Base.classes.greenhouse_gases
-power_plant_names = Base.classes.power_plant_names
-power_plant_gen_stats = Base.classes.power_plant_gen_stats
-power_plants_info = Base.classes.power_plants_info
+co = Base.classes.countries
+ghg = Base.classes.greenhouse_gases
+ppn = Base.classes.power_plant_names
+ppgs = Base.classes.power_plant_gen_stats
+ppi = Base.classes.power_plants_info
 
 # Route to render index.html template
 @app.route("/")
@@ -30,12 +30,88 @@ def home():
 
     return render_template("index.html")
    
+# Route to trigger initial vis view before any user input is selected
+@app.route("/global_view")
+def global_view():
+
+    session = Session(engine)
+
+    years = ['2013', '2014', '2015', '2016','2017', '2018', '2019']
+
+    global_dict ={
+        'years': years,
+        'generated_clean': [],
+        'estimated_clean': [],
+        'generated_dirty': [],
+        'estimated_dirty': []
+    }
+
+    gen_base = 'generation_gwh_'
+    est_base = 'estimated_generation_gwh_'
+
+    for year in years:
+        
+        # finding the sum of estimated totals 
+        try:
+            est_string = est_base + year
+            est_column = getattr(ppgs, est_string)
+
+            # clean results
+            results_clean = session.query(func.sum(est_column)).join(ppi, ppgs.plant_id == ppi.plant_id).filter(ppi.clean_energy == True).all()
+            
+            value_clean = results_clean[0][0]
+            
+            if value_clean is None:
+                
+                global_dict['estimated_clean'].append(None)
+                
+            else:
+                global_dict['estimated_clean'].append(float(value_clean))
+            
+            # non-clean results
+            results_dirty = session.query(func.sum(est_column)).join(ppi, ppgs.plant_id == ppi.plant_id).filter(ppi.clean_energy == False).all()
+            
+            value_dirty = results_dirty[0][0]
+            
+            if value_dirty is None:
+                
+                global_dict['estimated_dirty'].append(None)
+                
+            else:
+                global_dict['estimated_dirty'].append(float(value_dirty))
+            
+        except:
+            
+            global_dict['estimated_dirty'].append(None)
+            global_dict['estimated_clean'].append(None)
+        
+        # finding the sum of generated totals 
+        gen_string = gen_base + year
+        gen_column = getattr(ppgs, gen_string)
+        
+        # clean results
+        gen_results_clean = session.query(func.sum(gen_column)).join(ppi, ppgs.plant_id == ppi.plant_id).filter(ppi.clean_energy == True).all()
+        
+        gen_value_clean = float(gen_results_clean[0][0])
+        global_dict['generated_clean'].append(gen_value_clean)
+        
+        # non-clean results
+        gen_results_dirty = session.query(func.sum(gen_column)).join(ppi, ppgs.plant_id == ppi.plant_id).filter(ppi.clean_energy == False).all()
+        
+        gen_value_dirty = float(gen_results_dirty[0][0])
+        global_dict['generated_dirty'].append(gen_value_dirty)
+
+    session.close()
+
+    # return global totals
+    return jsonify(global_dict)
 
 # Route that will trigger the scrape function
 @app.route("/<country_name>")
 def update(country_name):
 
-    # return some json object
+    # return json object for specific country totals
+    return
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/logic.js
+++ static/logic.js
@@ -1,64 +1,118 @@
+var localHostURL = "http://127.0.0.1:5000/";
+var flaskGlobalViewURL = localHostURL + "global_view";
 
 
+d3.json(flaskGlobalViewURL).then(function (response) {
+
+    var trace_est_clean = {
+        x: response.years,
+        y: response.estimated_clean,
+        text: response.estimated_clean,
+        type: 'bar',
+        name: 'Estimated Clean Energy Generated'
+    };
+
+    var trace_gen_clean = {
+        x: response.years,
+        y: response.generated_clean,
+        text: response.generated_clean,
+        type: 'bar',
+        name: 'Reported Clean Energy Generated'
+    };
+
+    var trace_est_dirty = {
+        x: response.years,
+        y: response.estimated_dirty,
+        text: response.estimated_dirty,
+        type: 'bar',
+        name: 'Estimated Dirty Energy Generated'
+    };
+
+    var trace_gen_dirty = {
+        x: response.years,
+        y: response.generated_dirty,
+        text: response.generated_dirty,
+        type: 'bar',
+        name: 'Reported Dirty Energy Generated'
+    };
+
+    var barData = [trace_gen_clean, trace_gen_dirty, trace_est_clean, trace_est_dirty];
+
+    var layout = {
+        barmode: 'group',
+        // Add country name here
+        title: "Energy produced by power plants in the United States",
+        yaxis: {
+          title: "Gigawatt hours (GWh)"
+        },
+        xaxis: {
+          title: "Year"
+        },
+      };
+
+    Plotly.newPlot('bar', barData, layout);
+
+});
 
 
+// Trigger updatePlotly on a change to the dropdown menu value
+d3.selectAll("#selDataset").on("change", updatePlotly);
 
+// function createMap(markerLayer, countryLayer) {
 
-function createMap(markerLayer, countryLayer) {
+//     // Create the tile layer that will be the background of our map.
+//     var streetMapLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//     });
 
-    // Create the tile layer that will be the background of our map.
-    var streetMapLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    });
+//     var topoLayer = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+//         attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+//     });    
 
-    var topoLayer = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-        attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-    });    
-
-    var satelliteLayer = L.tileLayer.provider('MapBox', {
-        id: 'mapbox/satellite-v9',
-        accessToken: 'pk.eyJ1IjoiamFjb2J0cmV2aXRoaWNrMSIsImEiOiJja3c2eG45bjgzNGtoMnZwYXRnYjJzNGttIn0.3qRI1UQUwRpCWWo_uFk1Tw'
-    });
+//     var satelliteLayer = L.tileLayer.provider('MapBox', {
+//         id: 'mapbox/satellite-v9',
+//         accessToken: 'pk.eyJ1IjoiamFjb2J0cmV2aXRoaWNrMSIsImEiOiJja3c2eG45bjgzNGtoMnZwYXRnYjJzNGttIn0.3qRI1UQUwRpCWWo_uFk1Tw'
+//     });
     
 
-    var myMap = L.map('map', {
-        center: mapCenter,
-        zoom: mapZoom,
-        layers: [streetMapLayer, markerLayer, topoLayer, plateLayer, satelliteLayer]
-    });
+//     var myMap = L.map('map', {
+//         center: mapCenter,
+//         zoom: mapZoom,
+//         layers: [streetMapLayer, markerLayer, topoLayer, countryLayer, satelliteLayer]
+//     });
 
-    var baseMap = {
-        "Street Map": streetMapLayer,
-        "Topographical Map": topoLayer,
-        "Satellite Map": satelliteLayer
-    };
+//     var baseMap = {
+//         "Street Map": streetMapLayer,
+//         "Topographical Map": topoLayer,
+//         "Satellite Map": satelliteLayer
+//     };
 
-    var earthquakeOverlayMap = {
-        "Earthquakes": markerLayer,
-        "Tectonic Plates": plateLayer
-    };
+//     var earthquakeOverlayMap = {
+//         "Earthquakes": markerLayer,
+//         "Greenhouse Gases": countryLayer
+//     };
 
-    L.control.layers(baseMap, earthquakeOverlayMap, {
-        collapsed: false
-    }).addTo(myMap);
+//     L.control.layers(baseMap, earthquakeOverlayMap, {
+//         collapsed: false
+//     }).addTo(myMap);
 
-    var legend = L.control({position: 'bottomright'});
+//     var legend = L.control({position: 'bottomright'});
 
-    legend.onAdd = function (map) {
+//     legend.onAdd = function (map) {
 
-        var div = L.DomUtil.create('div', 'info legend'),
-            grades = [-10, 10, 30, 50, 70, 90],
-            labels = [];
+//         var div = L.DomUtil.create('div', 'info legend'),
+//             grades = [-10, 10, 30, 50, 70, 90],
+//             labels = [];
 
-        // loop through our density intervals and generate a label with a colored square for each interval
-        for (var i = 0; i < grades.length; i++) {
-            div.innerHTML +=
-                '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-        }
+//         // loop through our density intervals and generate a label with a colored square for each interval
+//         for (var i = 0; i < grades.length; i++) {
+//             div.innerHTML +=
+//                 '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+//                 grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+//         }
 
-        return div;
-    };
+//         return div;
+//     };
 
-    legend.addTo(myMap);
-};
+//     legend.addTo(myMap);
+// };
