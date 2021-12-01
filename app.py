5cf043a6bd980141273c06a2ea1f67ed286d4af7
Jacob Trevithick <jaketrevithick@Jakes-MBP.attlocal.net>
2021-11-30 19:11:43 -0800

    further organizing and merging plot.js with logic.js

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, redirect, jsonify
+import pandas as pd
 import config
-import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
+from sqlalchemy import create_engine, func, distinct
 
 # Create an instance of Flask
 app = Flask(__name__)
@@ -36,47 +36,53 @@ def global_view():
 
     session = Session(engine)
 
-    years = ['2013', '2014', '2015', '2016','2017', '2018', '2019']
+    pp_years = ['2013', '2014', '2015', '2016','2017', '2018', '2019']
 
-    global_dict ={
-        'years': years,
-        'generated_clean': [],
-        'estimated_clean': [],
-        'generated_dirty': [],
-        'estimated_dirty': []
+    global_dict = {
+        'pp_years': pp_years, # done
+        'generated_clean': [], # done
+        'estimated_clean': [], # done
+        'generated_dirty': [], # done
+        'estimated_dirty': [], # done
+        'clean_plant_count': [],
+        'dirty_plant_count': [],
+        'plant_labels': [],
+        'plant_counts_split': [],
+        'greenhouse_years': [],
+        'ghgs_totals': [],
+        'co2_totals': [],
+        'ch4_totals': [],
+        'n2o_totals': []
     }
 
     gen_base = 'generation_gwh_'
     est_base = 'estimated_generation_gwh_'
 
-    for year in years:
+    # total power production data for plants
+    for year in pp_years:
         
         # finding the sum of estimated totals 
         try:
             est_string = est_base + year
             est_column = getattr(ppgs, est_string)
 
-            # clean results
+            # clean estimated results
             results_clean = session.query(func.sum(est_column)).join(ppi, ppgs.plant_id == ppi.plant_id).filter(ppi.clean_energy == True).all()
             
             value_clean = results_clean[0][0]
             
             if value_clean is None:
-                
                 global_dict['estimated_clean'].append(None)
-                
             else:
                 global_dict['estimated_clean'].append(float(value_clean))
             
-            # non-clean results
+            # non-clean estimated results
             results_dirty = session.query(func.sum(est_column)).join(ppi, ppgs.plant_id == ppi.plant_id).filter(ppi.clean_energy == False).all()
             
             value_dirty = results_dirty[0][0]
             
             if value_dirty is None:
-                
                 global_dict['estimated_dirty'].append(None)
-                
             else:
                 global_dict['estimated_dirty'].append(float(value_dirty))
             
@@ -100,7 +106,50 @@ def global_view():
         
         gen_value_dirty = float(gen_results_dirty[0][0])
         global_dict['generated_dirty'].append(gen_value_dirty)
+            
+    # Power plant counts section here:
+    count_results_clean = session.query(func.count(ppi.plant_id)).filter(ppi.clean_energy == True).all()[0][0]
+    count_results_dirty = session.query(func.count(ppi.plant_id)).filter(ppi.clean_energy == False).all()[0][0]
 
+    global_dict['clean_plant_count'] = count_results_clean
+    global_dict['dirty_plant_count'] = count_results_dirty
+
+    # power plant counts distributed by count
+    pp_distribution_results = session.query(ppi.primary_fuel, func.count(ppi.primary_fuel)).group_by(ppi.primary_fuel).all()
+
+    pp_dist_df = pd.DataFrame(pp_distribution_results, columns = ['primary_fuel', 'count'])
+
+    global_dict['plant_labels'] = pp_dist_df['primary_fuel'].tolist()
+    global_dict['plant_counts_split'] = pp_dist_df['count'].tolist()
+
+
+    # Greenhouse gases data pulling here:
+    ghgs_years_results = session.query(distinct(ghg.year)).order_by(ghg.year).all()
+    ghgs_years_list = pd.DataFrame(ghgs_years_results, columns = ['years'])['years'].tolist()
+
+    global_dict['greenhouse_years'] = ghgs_years_list
+
+    # need to create function for this
+    ghgs_totals_results = session.query(func.sum(ghg.value)).filter(ghg.category_short == 'ghgs').group_by(ghg.year).order_by(ghg.year).all()
+    ghgs_totals_list = pd.DataFrame(ghgs_totals_results, columns = ['totals'])['totals'].tolist()
+    ghgs_totals_floats = [float(x) for x in ghgs_totals_list]
+    global_dict['ghgs_totals'] = ghgs_totals_floats
+
+    co2_totals_results = session.query(func.sum(ghg.value)).filter(ghg.category_short == 'co2').group_by(ghg.year).order_by(ghg.year).all()
+    co2_totals_list = pd.DataFrame(co2_totals_results, columns = ['totals'])['totals'].tolist()
+    co2_totals_floats = [float(x) for x in co2_totals_list]
+    global_dict['co2_totals'] = co2_totals_floats
+
+    ch4_totals_results = session.query(func.sum(ghg.value)).filter(ghg.category_short == 'ch4').group_by(ghg.year).order_by(ghg.year).all()
+    ch4_totals_list = pd.DataFrame(ch4_totals_results, columns = ['totals'])['totals'].tolist()
+    ch4_totals_floats = [float(x) for x in ch4_totals_list]
+    global_dict['ch4_totals'] = ch4_totals_floats
+
+    n2o_totals_results = session.query(func.sum(ghg.value)).filter(ghg.category_short == 'n2o').group_by(ghg.year).order_by(ghg.year).all()
+    n2o_totals_list = pd.DataFrame(n2o_totals_results, columns = ['totals'])['totals'].tolist()
+    n2o_totals_floats = [float(x) for x in n2o_totals_list]
+    global_dict['n2o_totals'] = n2o_totals_floats
+    
     session.close()
 
     # return global totals
--- static/logic.js
+++ static/logic.js
@@ -3,9 +3,9 @@ var flaskGlobalViewURL = localHostURL + "global_view";
 
 
 d3.json(flaskGlobalViewURL).then(function (response) {
-
+    
     var trace_est_clean = {
-        x: response.years,
+        x: response.pp_years,
         y: response.estimated_clean,
         text: response.estimated_clean,
         type: 'bar',
@@ -38,25 +38,186 @@ d3.json(flaskGlobalViewURL).then(function (response) {
 
     var barData = [trace_gen_clean, trace_gen_dirty, trace_est_clean, trace_est_dirty];
 
+    var region = 'Global'
+
     var layout = {
         barmode: 'group',
         // Add country name here
-        title: "Energy produced by power plants in the United States",
+        title: `Energy produced by power plants in the ${region}`,
         yaxis: {
-          title: "Gigawatt hours (GWh)"
+        title: "Gigawatt hours (GWh)"
         },
         xaxis: {
-          title: "Year"
+        title: "Year"
         },
-      };
+    };
 
     Plotly.newPlot('bar', barData, layout);
 
+    // Stacked line plot
+    var trace1 = {
+        x: response.greenhouse_years,
+        y: response.n2o_totals,
+        fill: 'tozeroy',
+        name: "Nitrous oxide",
+        type: 'scatter',
+        mode: 'none'
+    };
+    
+    var trace2 = {
+        x: response.greenhouse_years,
+        y: response.ch4_totals,
+        fill: 'tonexty',
+        name: "Methane",
+        type: 'scatter',
+        mode: 'none'
+    };
+    
+    var trace3 = {
+        x: response.greenhouse_years,
+        y: response.co2_totals,
+        fill: 'tonexty',
+        name: "Carbon dioxide",
+        type: 'scatter',
+        mode: 'none'
+    };
+    
+    var trace4 = {
+        x: response.greenhouse_years,
+        y: response.ghgs_totals,
+        fill: 'tonexty',
+        name: "Total GHG emissions",
+        type: 'scatter',
+        mode: 'none'
+    };
+    
+    
+    var layout = {
+        title: 'Historic GHG emission from electricity production',
+        height: 600,
+        width: 1200,
+        yaxis: {
+        title: "kilotonne CO2 equivalent"
+        },
+        xaxis: {
+        title: "Year"
+        },
+    };
+    
+    var data = [trace1, trace2, trace3, trace4];
+    
+    Plotly.newPlot('gauge', data, layout);
+
+    var data = [{
+        type: "pie",
+        values: [response.clean_plant_count, response.dirty_plant_count],
+        labels: ["Clean power plants", "Dirty power plants"],
+        marker: {colors: ['#8FBC8F','#696969']},
+        textinfo: "label+percent",
+        textposition: "outside",
+        automargin: true
+      }]
+      
+      var layout = {
+        height: 400,
+        width: 400,
+        margin: {"t": 0, "b": 0, "l": 0, "r": 0},
+        showlegend: false
+        }
+      
+      Plotly.newPlot('bar', data, layout)
+
+
+      const radarData = {
+        labels: response.plant_labels,
+        datasets: [{
+          label: 'Number of Total Power Plants',
+          data: response.plant_counts_split,
+          fill: true,
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgb(255, 99, 132)',
+          pointBackgroundColor: 'rgb(255, 99, 132)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgb(255, 99, 132)'
+        }]
+      };
+      
+      const config = {
+        type: 'radar',
+        data: radarData,
+        options: {
+          elements: {
+            line: {
+              borderWidth: 3
+            }
+          }
+        },
+      };
+      
+      const radar = new Chart(
+        document.getElementById('radar'),
+        config
+      );
 });
 
 
 // Trigger updatePlotly on a change to the dropdown menu value
-d3.selectAll("#selDataset").on("change", updatePlotly);
+// d3.selectAll("#selDataset").on("change", updatePlotly);
+
+
+// //     var trace_est_clean = {
+//     x: response.years,
+//     y: response.estimated_clean,
+//     text: response.estimated_clean,
+//     type: 'bar',
+//     name: 'Estimated Clean Energy Generated'
+// };
+
+// var trace_gen_clean = {
+//     x: response.years,
+//     y: response.generated_clean,
+//     text: response.generated_clean,
+//     type: 'bar',
+//     name: 'Reported Clean Energy Generated'
+// };
+
+// var trace_est_dirty = {
+//     x: response.years,
+//     y: response.estimated_dirty,
+//     text: response.estimated_dirty,
+//     type: 'bar',
+//     name: 'Estimated Dirty Energy Generated'
+// };
+
+// var trace_gen_dirty = {
+//     x: response.years,
+//     y: response.generated_dirty,
+//     text: response.generated_dirty,
+//     type: 'bar',
+//     name: 'Reported Dirty Energy Generated'
+// };
+
+// var barData = [trace_gen_clean, trace_gen_dirty, trace_est_clean, trace_est_dirty];
+
+// var layout = {
+//     barmode: 'group',
+//     // Add country name here
+//     title: "Energy produced by power plants in the United States",
+//     yaxis: {
+//       title: "Gigawatt hours (GWh)"
+//     },
+//     xaxis: {
+//       title: "Year"
+//     },
+//   };
+
+// Plotly.newPlot('bar', barData, layout);
+
+
+
+
+
 
 // function createMap(markerLayer, countryLayer) {
 
